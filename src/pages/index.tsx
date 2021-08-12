b5bf28b022a81925ab00366364ef4f1cc721c62f
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-12 06:47:24 -0300

    Trabalhando na estrutura do meu componente signinbutton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import  Head  from "next/head";
 
-
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
       <h1>Hello World</h1>
     </>
