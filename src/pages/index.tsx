3a227828192797320d242df183767cbbc72ab55b
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-12 05:08:27 -0300

    Configurando fonte externa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import styled from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <h1 className={styled.title}>Hello World Next</h1>
+    <h1 className={styled.title}>
+      Hello World Next
+      <span>Title</span>
+    </h1>
   )
 }
