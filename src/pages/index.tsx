cf41f3cf84000e5a05674eb610426503551ef2f9
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-15 14:12:52 -0300

    Adicionando a funcionalidade de server side render dentro de nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,14 @@ import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home(props) {
-  console.log(props)
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }) {
   return (
     <>
       <Head>
@@ -17,9 +23,9 @@ export default function Home(props) {
           <span>👏Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
@@ -27,12 +33,19 @@ export default function Home(props) {
   )
 }
 
-
 export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe
+  const price = await stripe.prices.retrieve('price_1JNbJ8D1zEniB983iwPXHh4S');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
   return {
    props: {
-     name: 'Pedro'
+     product
    } 
   }
 }
