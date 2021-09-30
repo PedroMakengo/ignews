3a80ae74a09f6a4906460de3e89a3343b5505b24
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-30 05:11:34 -0300

    Zerando as configurações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
-import { GetStaticProps } from "next";
 import Head from "next/head";
-import { SubscribeButton } from "../components/SubscribeButton";
-import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
-
-interface HomeProps {
-  product: {
-    priceId: string;
-    amount: number;
-  };
-}
 
 export default function Home({ product }) {
   return (
@@ -26,30 +16,11 @@ export default function Home({ product }) {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for {product.amount} month</span>
+            <span>for month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve("price_1JNbJ8D1zEniB983iwPXHh4S");
-
-  const product = {
-    priceId: price.id,
-    amount: new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(price.unit_amount / 100),
-  };
-  return {
-    props: {
-      product,
-    },
-    revalidate: 60 * 60 * 24, // 24 Hours
-  };
-};
