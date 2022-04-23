a15abbf2ed710f797a2dfdc76bb31ec832568744
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-23 10:54:44 -0300

    Trabalhando com a minha estilização global

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>ig.news</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+          />
         </Head>
         <body>
           <Main />
