bc5e7e5c40c927d8552c28c70b3c8aeabe424e19
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-23 15:18:44 -0300

    Terminando com os componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
@@ -6,9 +7,8 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <Header />
+      <h1>Hello World</h1>
     </>
   );
 }
