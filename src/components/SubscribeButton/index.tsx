1b31dbbee8535f9cf13e68c6338dac350b9d8d87
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-04 09:19:01 +0100

    Trabalhando

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,27 @@
 import styles from "./styles.module.scss";
+import { signIn, useSession } from "next-auth/client";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    // Criação da checkout session
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
