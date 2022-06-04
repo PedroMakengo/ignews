08c232321a64b7a4c06a5b79a4732ea34e8a7ef3
PedroMakengo <pedromakengomiguel@gmail.com>
2022-06-04 15:08:09 +0100

    Trabalhando com a parte do usuário

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { signIn, useSession } from "next-auth/client";
-import {api} from '../../services/api'
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -17,9 +18,15 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
     // Criação da checkout session
     try {
-      const response = await api.post('/subscribe');
+      const response = await api.post("/subscribe");
 
-      const {sessionId} = response.data;
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   }
   return (
